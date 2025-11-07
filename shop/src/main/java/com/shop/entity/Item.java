8cbf32a8eafd3cfa6260e55e350fca48e58bcb69
CheongMinsang <lgu33993388@gmail.com>
2025-11-07 13:18:25 +0900

    Repository 설계

--- shop/src/main/java/com/shop/entity/Item.java
+++ shop/src/main/java/com/shop/entity/Item.java
@@ -1,23 +1,41 @@
 package com.shop.entity;
 
 import com.shop.constant.ItemSellStatus;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "item")
 @Getter
 @Setter
 @ToString
 public class Item {
 
+    @Id
+    @Column(name = "item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id; //商品コード
+
+    @Column(nullable = false, length = 50)
     private String itemNm; //商品名
+
+    @Column(name = "price", nullable = false)
     private int price; //値段
+
+    @Column(nullable = false)
     private int stockNumber; //在庫数
-    private String itemDetail; //商品詳細説明
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail; //商品詳細説明c
+
+    @Enumerated(EnumType.STRING)
     private ItemSellStatus itemSellStatus; //商品販売状態
+
     private LocalDateTime regTime; //登録時刻
     private LocalDateTime updateTime; //修正時刻
 }
